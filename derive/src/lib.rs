6de6a78d5f4ca2f7e9cabb84076a1de22a11211c
Kagami Sascha Rosylight <saschanaz@outlook.com>
2022-12-30 15:51:24 +0900

    Enum support

--- derive/src/lib.rs
+++ derive/src/lib.rs
@@ -4,47 +4,85 @@ use syn::*;
 #[macro_use]
 extern crate quote;
 
-pub(crate) fn gen_impl(ast: &syn::DeriveInput) -> Result<TokenStream> {
+fn generate_struct(
+    id: &Ident,
+    generics: &Generics,
+    data_struct: &DataStruct,
+) -> Result<TokenStream> {
+    let field_ids: Vec<_> = data_struct
+        .fields
+        .iter()
+        .map(|field| {
+            let id = field
+                .ident
+                .as_ref()
+                .expect("Tuple struct not supported yet");
+            quote! { #id }
+        })
+        .collect();
+    let field_parsers = data_struct.fields.iter().map(|field| {
+        let ty = &field.ty;
+        quote! { weedle!(#ty) }
+    });
+
+    let result = quote! {
+        impl<'a> crate::Parse<'a> for #id #generics {
+            fn parse(input: &'a str) -> crate::IResult<&'a str, Self> {
+                use nom::lib::std::result::Result::Ok;
+                let (input, (#(#field_ids),*)) = nom::sequence::tuple((
+                    #(#field_parsers,)*
+                ))(input)?;
+
+                Ok((input, Self {
+                    #(#field_ids),*
+                }))
+            }
+        }
+    };
+
+    // eprintln!("\n***\nglobal_impl: {}\n---\n", result);
+
+    Ok(result.into())
+}
+
+fn generate_enum(id: &Ident, generics: &Generics, data_enum: &DataEnum) -> Result<TokenStream> {
+    let field_parsers = data_enum.variants.iter().map(|variant| {
+        let variant_id = &variant.ident;
+        let fields = match &variant.fields {
+            Fields::Unnamed(unnamed) => unnamed,
+            _ => panic!("Only tuple variant enums are supported"),
+        };
+        if fields.unnamed.len() != 1 {
+            panic!("Only one tuple field is supported");
+        }
+        let field = fields.unnamed.first().unwrap();
+        let ty = &field.ty;
+
+        quote! { weedle!(#ty).map(#id::#variant_id) }
+    });
+
+    let result = quote! {
+        impl<'a> crate::Parse<'a> for #id #generics {
+            fn parse(input: &'a str) -> crate::IResult<&'a str, Self> {
+                use nom::Parser;
+                nom::branch::alt((
+                    #(#field_parsers),*
+                ))(input)
+            }
+        }
+    };
+
+    // eprintln!("\n***\nglobal_impl: {}\n---\n", result);
+
+    Ok(result.into())
+}
+
+fn generate(ast: &syn::DeriveInput) -> Result<TokenStream> {
     let id = &ast.ident;
     let generics = &ast.generics;
     match &ast.data {
-        syn::Data::Struct(data_struct) => {
-            let field_ids: Vec<_> = data_struct
-                .fields
-                .iter()
-                .map(|field| {
-                    let id = field
-                        .ident
-                        .as_ref()
-                        .expect("Tuple struct not supported yet");
-                    quote! { #id }
-                })
-                .collect();
-            let field_parsers = data_struct.fields.iter().map(|field| {
-                let ty = &field.ty;
-                quote! { weedle!(#ty) }
-            });
-
-            let result = quote! {
-                impl<'a> crate::Parse<'a> for #id #generics {
-                    fn parse(input: &'a str) -> crate::IResult<&'a str, Self> {
-                        use nom::lib::std::result::Result::Ok;
-                        let (input, (#(#field_ids),*)) = nom::sequence::tuple((
-                            #(#field_parsers),*
-                        ))(input)?;
-
-                        Ok((input, Self {
-                            #(#field_ids),*
-                        }))
-                    }
-                }
-            };
-
-            // eprintln!("\n***\nglobal_impl: {}\n---\n", result);
-
-            Ok(result.into())
-        }
-        syn::Data::Enum(_) => panic!("Enums not supported yet"),
+        syn::Data::Struct(data_struct) => generate_struct(id, generics, data_struct),
+        syn::Data::Enum(data_enum) => generate_enum(id, generics, data_enum),
         syn::Data::Union(_) => panic!("Unions not supported"),
     }
 }
@@ -55,7 +93,7 @@ pub fn weedle(input: TokenStream) -> TokenStream {
     let ast = parse_macro_input!(input as DeriveInput);
 
     // Build and return the generated impl
-    match gen_impl(&ast) {
+    match generate(&ast) {
         Ok(ts) => ts,
         Err(e) => e.to_compile_error().into(),
     }
